c4ffe66bb3b54628a99e3ec54d7cee9b2c34421f
David Weese <dave.weese@gmail.com>
2022-04-21 02:54:04 +0200

    remount

--- cross-build/build-kernel.sh
+++ cross-build/build-kernel.sh
@@ -103,6 +103,9 @@ execute "kpartx -a -v -s $OUTFILE"
 
 # Resize fs
 execute "growpart /dev/loop0 2"
+execute "kpartx -d /dev/loop0 || true"
+execute "losetup -d /dev/loop0 || true"
+execute "kpartx -a -v -s $OUTFILE"
 execute "e2fsck -y -f /dev/mapper/loop0p2"
 execute "resize2fs /dev/mapper/loop0p2"
 
